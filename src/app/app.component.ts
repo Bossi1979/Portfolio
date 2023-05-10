f06083c719186260b29a650c60ccd92898bad8c4
Stefan Boskamp <stboskamp@gmail.com>
2023-05-10 17:56:49 +0200

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,13 @@
-import { Component, OnInit, OnChanges} from '@angular/core';
-// import {LayoutModule} from '@angular/cdk/layout';
-import {BreakpointObserver} from '@angular/cdk/layout';
-
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Portfolio';
-  // layout = new LayoutModule;
-
-  constructor(
-    // public layout: LayoutModule,
-    public breakpoints: BreakpointObserver,
-
-  ){
-
-  }
-
-  ngOnInit() {
-    if(this.breakpoints.isMatched('(max-width: 800px)')){
-      console.log('Width less than 800px');
-    }
-    
-  }
-
   
-    
 }
 
 
